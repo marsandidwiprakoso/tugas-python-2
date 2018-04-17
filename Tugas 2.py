d75de310c3216bc8ef105bfe2ebeb5aec6a5c13a
marsandidwiprakoso <37301176+marsandidwiprakoso@users.noreply.github.com>
2018-04-17 02:56:28 +0000

    Add files via upload

---
+++ Tugas 2.py
@@ -0,0 +1,13 @@
+Nama ="Abdul Rahman Cholik"
+print "\tNama :",Nama
+Nim = 311710179
+print "\tNim :",Nim
+NilaiTugas = 95
+print "\tNilai Tugas :",NilaiTugas
+Uts = 100
+print "\tUts :",Uts
+Uas = 80
+print "\tUas :",Uas
+
+        
+	
